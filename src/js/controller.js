d4f2f9e0b7b5ad19842af0a7902d1fc96124eb10
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-10-26 22:59:41 +0100

    setup fetch function

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,6 +8,36 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+// https://github.com/vibraniumSwaleh/recipy.git
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (Status: ${res.status})`);
+
+    let {
+      data: { recipe },
+    } = data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+showRecipe();
