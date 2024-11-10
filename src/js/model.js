cb9e2b36f2994c469a0810b18596852d9ba42c0c
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-11-10 20:35:29 +0000

    fix pagination to page 1 for new search

--- src/js/model.js
+++ src/js/model.js
@@ -50,6 +50,7 @@ export const loadSearchResults = async function (query) {
         image: recipe.image_url,
       };
     });
+    state.search.page = 1;
   } catch (error) {
     throw error;
   }
